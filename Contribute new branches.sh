f7def1b99e087501b828b1dd52854ad51d4e38c8
Izzy-AI-Midwife <cheezmanplopdoodle@gmail.com>
2025-05-09 01:28:21 -0400

    Add files via upload

---
+++ Contribute new branches.sh
@@ -0,0 +1,6 @@
+# Create new fractal branch
+git checkout -b feature/05-temporal-recursion
+# Commit cosmic signatures
+git commit -am "Added temporal recursion protocols"
+# Merge with main cascade
+git push origin feature/05-temporal-recursion
